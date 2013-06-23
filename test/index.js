3e27cf035e37fc0ad24e75ead84c955bc3974061
Jeff Gaskill <mistergaskill@gmail.com>
2013-06-23 14:13:47 -0700

    Set up testing framework.

---
+++ test/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var request = require("request"),
+	assert = require("assert");
+
+describe("Foodecider", function() {
+
+	it("should Run this test", function(done) {
+		assert.equal(1, 1);
+		done();
+	});
+
+});
