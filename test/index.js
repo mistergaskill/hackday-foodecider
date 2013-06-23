c41657953e31454d7c3a391334cd96e8497c35f1
Jeff Gaskill <mistergaskill@gmail.com>
2013-06-23 14:27:59 -0700

    Updated simple first test.

--- test/index.js
+++ test/index.js
@@ -1,13 +1,26 @@
 "use strict";
 
 var request = require("request"),
-	assert = require("assert");
+	assert = require("assert"),
+	API_URL = "http://localhost:8000";
 
 describe("Foodecider", function() {
-
 	it("should Run this test", function(done) {
-		assert.equal(1, 1);
-		done();
-	});
+		request.post(API_URL + "/", {
+			people: [
+				{name: "jeff", phone: "9492370653"},
+				{name: "parsha", phone: "9492301333"},
+			],
+		}, function(err, res, body) {
+			assert.ifError(err);
+			assert.equal(res.status, 200);
+			assert.ok(body.sessionID);
+			assert.ok(body.texted);
+			assert.ok(body.failed);
 
+			assert.equal(body.texted.length, 2);
+			assert.equal(body.failed.length, 2);
+			done();
+		});
+	});
 });
